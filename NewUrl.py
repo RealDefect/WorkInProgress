53cb873cbf7b24be506a2e98ed52bcc07a2d2165
RealDefect <34613759+RealDefect@users.noreply.github.com>
2018-03-19 14:17:24 -0500

    update to code

--- NewUrl.py
+++ NewUrl.py
@@ -1,7 +1,15 @@
 import webbrowser
-
+import os
 #Launch website and input url 
 UrlInput = input('Enter URL')
 
 webbrowser.open (UrlInput)
 
+
+#interact with website
+
+print('Succesfully opened browser')
+
+
+#Closing webbrowser
+os.system ("taskkill /im iexplore.exe /f")
