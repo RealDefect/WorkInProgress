bf479ca437fc77d774277377931df09cb6f83758
RealDefect <34613759+RealDefect@users.noreply.github.com>
2018-03-19 21:20:56 -0500

    Updating added UrlInput_2 and added check aganist it

---
+++ WebSurfer/WebSurfer/NewUrl.py
@@ -0,0 +1,20 @@
+import webbrowser
+import os
+import html
+
+#Launch website and input url 
+UrlInput = input('Enter URL')
+
+webbrowser.open (UrlInput)
+UrlInput_2= setattr.__self_(webbrowser)
+
+
+#interact with website
+if UrlInput = UrlInput_2:print('Succesfully opened browser')
+else print('Unsucessfully opened brower')
+End If
+
+
+
+#Closing webbrowser
+os.system ("taskkill /im iexplore.exe /f")
